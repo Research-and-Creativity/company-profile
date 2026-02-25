ea34e78a73a26cb983ac433a66d74ea0b4e59e6c
Geusan Edurais <edurais.ariadaffa@gmail.com>
2026-02-25 12:20:05 +0700

    features: add reusable button component with types

---
+++ src/components/ui/Button.tsx
@@ -0,0 +1,12 @@
+interface ButtonProps {
+    text: string;
+    classAdd?: string;
+}
+
+export default function Button({ text, classAdd = "" }: ButtonProps) {
+    return (
+        <button className={`py-3 px-8 cursor-pointer hover:bg-blue-700 text-white bg-linear-to-r from-[#040850] to-[#218ABB] rounded-full font-bold transition-all shadow-lg shadow-blue-600/20 ${classAdd}`}>
+            {text}
+        </button>
+    )
+}
