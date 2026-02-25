b3fd78359214263fb57d5f3895049a95942fb295
Geusan Edurais <edurais.ariadaffa@gmail.com>
2026-02-25 11:45:41 +0700

    features: add section grouping file

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,33 @@
+import About from "./section/About";
+import Contact from "./section/Contact";
+import Hero from "./section/Hero";
+import OurProducts from "./section/OurProducts";
+import OurServices from "./section/OurServices";
+
+const HomePage = () => {
+    return (
+        <div className="flex flex-col w-full">
+            <section id="home">
+                <Hero />
+            </section>
+
+            <section id="about" className="py-20">
+                <About />
+            </section>
+
+            <section id="services" className="py-20 bg-slate-50/50">
+                <OurServices />
+            </section>
+
+            <section id="products" className="py-20">
+                <OurProducts />
+            </section>
+
+            <section id="contact" className="py-20 bg-[#001534] text-white">
+                <Contact />
+            </section>
+        </div>
+    );
+};
+
+export default HomePage;
