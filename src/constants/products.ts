bb55fc4d52c7cd66b954ae2c9266597ed5437b63
Geusan Edurais <edurais.ariadaffa@gmail.com>
2026-02-26 13:25:34 +0700

    feature: add product card

---
+++ src/constants/products.ts
@@ -0,0 +1,36 @@
+import type { Product } from "../types/product";
+
+export const Products: Product[] = [
+  {
+    id: 1,
+    title: "Undangan Digital - Rustic Brown",
+    category: "Undangan Digital",
+    description:
+      "Layanan pembuatan undangan digital modern dan responsif untuk berbagai acara.",
+    image: "/assets/images/products/invitation-1.jpg",
+  },
+  {
+    id: 2,
+    title: "Undangan Digital - Elegant Black",
+    category: "Undangan Digital",
+    description:
+      "Layanan pembuatan undangan digital modern dan responsif untuk berbagai acara.",
+    image: "/assets/images/products/invitation-2.jpg",
+  },
+  {
+    id: 3,
+    title: "Website Company Profile",
+    category: "Website Development",
+    description:
+      "Layanan pembuatan website profesional untuk membangun kepercayaan pelanggan.",
+    image: "/assets/images/products/crypto-site.jpg",
+  },
+  {
+    id: 4,
+    title: "Modern Wedding Template",
+    category: "Undangan Digital",
+    description:
+      "Layanan pembuatan undangan digital modern dan responsif untuk berbagai acara.",
+    image: "/assets/images/products/invitation-3.jpg",
+  },
+];
