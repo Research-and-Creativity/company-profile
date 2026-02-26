c88833ca33c43e210f62a644120c77d9fbf12439
Geusan Edurais <edurais.ariadaffa@gmail.com>
2026-02-26 14:35:58 +0700

    update bg navbar

--- src/components/shared/Navbar.tsx
+++ src/components/shared/Navbar.tsx
@@ -1,5 +1,5 @@
-import { useState, useEffect } from 'react';
-import { NAV_LINKS } from '../../constants/navigation';
+import { useState, useEffect } from "react";
+import { NAV_LINKS } from "../../constants/navigation";
 import aboutImage from "../../assets/logo.svg";
 
 const Navbar = () => {
@@ -9,28 +9,32 @@ const Navbar = () => {
     const handleScroll = () => {
       setIsScrolled(window.scrollY > 20);
     };
-    window.addEventListener('scroll', handleScroll);
-    return () => window.removeEventListener('scroll', handleScroll);
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
   }, []);
 
   return (
-    <nav className={`fixed top-0 w-full z-50 transition-all duration-300 ${isScrolled ? 'bg-white/80 backdrop-blur-md shadow-sm py-3' : 'bg-transparent py-5'
-      }`}>
+    <nav
+      className={`fixed bg-white top-0 w-full z-50 transition-all duration-300 ${
+        isScrolled ? " backdrop-blur-md shadow-sm py-3" : "py-5"
+      }`}
+    >
       <div className="container mx-auto px-6 flex justify-between items-center">
         {/* Logo Zetech */}
-        
-        <div className="flex items-center gap-3">
-        {/* Logo Zetech */}
-        <img
-          src={aboutImage}
-          alt="Zetech Logo"
-          className="w-12 h-12 object-contain"
-        />
+        <a href="/">
+          <div className="flex items-center gap-3">
+            {/* Logo Zetech */}
+            <img
+              src={aboutImage}
+              alt="Zetech Logo"
+              className="w-12 h-12 object-contain"
+            />
 
-        <div className="text-2xl font-bold text-[#001534]">
-          ZE<span className="text-blue-600">TECH</span>
-        </div>
-      </div>
+            <div className="text-2xl font-bold text-[#001534]">
+              ZE<span className="text-blue-600">TECH</span>
+            </div>
+          </div>
+        </a>
 
         {/* Desktop Menu */}
         <div className="hidden md:flex gap-8">
