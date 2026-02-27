50d160e916d4dd0b81e9a268fac44946b85c1236
Geusan Edurais <edurais.ariadaffa@gmail.com>
2026-02-27 10:24:55 +0700

    ui: update button hover animation bg fill from rigth to left

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -6,9 +6,21 @@ interface ButtonProps {
 export default function Button({ text, classAdd = "" }: ButtonProps) {
   return (
     <button
-      className={`py-3 px-8 cursor-pointer text-white bg-linear-to-r from-[#040850] to-[#218ABB] rounded-full font-bold transition-all shadow-lg shadow-blue-600/20 ${classAdd}`}
+      className={`
+        relative group overflow-hidden py-3 px-8 cursor-pointer text-white 
+        bg-linear-to-r from-[#040850] to-[#218ABB] 
+        rounded-full font-bold transition-all shadow-lg shadow-blue-600/20
+        ${classAdd}
+      `}
     >
-      {text}
+      <div
+        className="
+          absolute top-0 bottom-0 w-0 bg-[#218abb] transition-all duration-500 ease-out 
+          group-hover:w-full -z-10 right-0
+        "
+      />
+
+      <span className="relative z-10">{text}</span>
     </button>
   );
 }
