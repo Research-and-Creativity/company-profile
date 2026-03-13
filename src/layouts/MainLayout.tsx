590cb9de1bc2c17ece98239df0982e5b1b23c6de
Geusan Edurais <edurais.ariadaffa@gmail.com>
2026-03-13 11:17:16 +0700

    feat: integrate pageview tracking with @vercel/analytics in MainLayout

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,8 +1,16 @@
-import { Outlet } from 'react-router-dom';
+import { Outlet, useLocation } from 'react-router-dom';
 import Navbar from '../components/shared/Navbar';
 import Footer from '../components/shared/Footer';
+import { useEffect } from 'react';
+import { track } from '@vercel/analytics';
 
 const MainLayout = () => {
+  const location = useLocation();
+
+  useEffect(() => {
+    track('pageview', { url: location.pathname });
+  }, [location]);
+  
   return (
     <div className="w-full flex flex-col min-h-screen">
       <Navbar />
